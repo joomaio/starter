503df196d6ac7105ffa5d2bab8f65935ab2a2d52
joomaio.huylq <dauxotcachua99@gmail.com>
2024-01-31 15:18:19 +0700

    create super user when install pnote

--- php_modules/devtool/starter/views/layouts/starter/list/row.php
+++ php_modules/devtool/starter/views/layouts/starter/list/row.php
@@ -3,7 +3,10 @@
         <div class="card-body">
             <h4 class="card-title"><?php echo $this->item['name'] ?></h4>
             <p class="card-text"><?php echo $this->item['description'] ?></p>
-            <div class="text-end">  
+            <div class="d-flex justify-content-end">  
+                <?php if (array_key_exists('button-create-user', $this->item)): ?> 
+                    <?php echo $this->renderWidget($this->item['button-create-user']['widget']); ?>
+                <?php endif; ?>
                 <button data-name="<?php echo $this->item['name']; ?>" data-code="<?php echo $this->item['code']; ?>" class="btn <?php echo $this->item['status'] ? 'btn-secondary btn-uninstall' : 'btn-primary btn-install' ?> ">
                     <?php echo $this->item['status'] ? 'Uninstall' : 'Install' ?>
                 </button>
