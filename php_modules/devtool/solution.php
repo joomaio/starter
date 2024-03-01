3a4dfb7e76c207c07c2318f842530a3fb509c164
tinh chu <cvtinh2000@gmail.com>
2024-03-01 11:35:00 +0700

    create solution php

---
+++ php_modules/devtool/solution.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'tags' => [],
+    'type' => 'solution',
+    'solution' => 'devtool',
+    'folder_name' => 'devtool',
+    'name' => 'Solution Dev Tool',
+    'dependencies' => [],
+];
