39e1816a42f8e4ca3ecbfb5c877a108b0dc1182f
tinh chu <cvtinh2000@gmail.com>
2024-03-07 17:00:45 +0700

    update theme controller

--- php_modules/devtool/starter/controllers/theme.php
+++ php_modules/devtool/starter/controllers/theme.php
@@ -8,29 +8,11 @@ class theme extends ControllerMVVM
 {
     public function install()
     {
-        $starter = $this->config->starter;
-
-        if (
-            !$starter || (!isset($starter['access_key']) || $starter['access_key'] == '') ||
-            (!isset($starter['username']) || $starter['username'] == '') || (!isset($starter['password']) || $starter['password'] == '')
-        ) {
-            $this->app->raiseError('Invalid request!');
-        }
-
-        $access_key = $this->request->get->get('access_key', '', 'string');
-        $user = $this->StarterAccessModel->user();
-        if (!$user) {
-            return $this->app->redirect(
-                $this->router->url('starter/login?access_key=' . $access_key)
-            );
-        }
-
-        $this->app->set('format', 'html');
-        $this->app->set('layout', 'starter.list');
+       
     }
 
     public function uninstall()
     {
-        $try = 
+        
     }
 }
