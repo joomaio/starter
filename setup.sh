792769d1de94fa8df5daf9984e5e4e261cd0a224
Tinh chu <cvtinh2000@gmail.com>
2024-07-09 00:20:57 +0700

    command check git version

--- setup.sh
+++ setup.sh
@@ -25,7 +25,24 @@ fi
 current_version=$(echo "$composer_version" | grep -oP 'Composer version \K[^\s]+')
 required_version="2.0"
 if [ "$(printf '%s\n' "$required_version" "$current_version" | sort -V | head -n1)" = "$required_version" ]; then 
-  echo "Composer version is >= 2.0"
+    # next to step 3
 else
-  echo "Composer version is < 2.0"
+    echo "Composer version is < 2.0"
+fi
+
+## Step 3 check git
+git_version=$(git --version 2>&1)
+
+if [ $? -ne 0 ]; then
+    echo "Git is not installed!"
+    exit 1
+fi
+
+current_version=$(echo "$git_version" | grep -oP 'git version \K[^\s]+')
+
+required_version="2"
+if [ "$(printf '%s\n' "$required_version" "$current_version" | sort -V | head -n1)" = "$required_version" ]; then 
+    # next to step 4
+else
+    echo "Git version is < 2"
 fi
