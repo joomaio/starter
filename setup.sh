d054d6da407f182c774ff7f297b3aac39f694149
Tinh chu <cvtinh2000@gmail.com>
2024-07-09 00:26:25 +0700

    composer install

--- setup.sh
+++ setup.sh
@@ -42,7 +42,20 @@ current_version=$(echo "$git_version" | grep -oP 'git version \K[^\s]+')
 
 required_version="2"
 if [ "$(printf '%s\n' "$required_version" "$current_version" | sort -V | head -n1)" = "$required_version" ]; then 
+    echo "Environment is ready"
     # next to step 4
 else
     echo "Git version is < 2"
 fi
+
+## Step 4 composer install
+composer require smpleader/spt smpleader/dtm
+
+if [ $? -eq 0 ]; then
+    echo "Composer install done!"
+else
+    echo "An error occurred while installing the packages."
+    exit 1
+fi
+
+## Step 5 Base Structure Setup
